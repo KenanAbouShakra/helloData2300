05cf15465ca2d488426447f4ab50e102096cf194
Kenanshakra <89753703+Kenanshakra@users.noreply.github.com>
2022-08-28 19:31:45 +0200

    Ny telling

--- src/Hellodata2300.java
+++ src/Hellodata2300.java
@@ -3,6 +3,10 @@ public class Hellodata2300 {
         System.out.println("tallene fra 0 til 10: ");
         for(int i=0; i<=10; i++){
             System.out.println(i);
+
         }
+        System.out.println("tallene nå fra 11 til 20: ");
+        for(int i=11; i<=20; i++){
+            System.out.println(i);}
     }
 }
