120e6df2912de6d5c8acad88abead744305f1ab9
Kenanshakra <89753703+Kenanshakra@users.noreply.github.com>
2022-08-28 19:06:55 +0200

    Initial commit

---
+++ src/Hellodata2300.java
@@ -0,0 +1,8 @@
+public class Hellodata2300 {
+    public static void main(String[] args) {
+        System.out.println("tallene fra 0 til 10: ");
+        for(int i=0; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
